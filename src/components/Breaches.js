e723e9e49836ae2a2b32a2461007490a000ee0de
Shani Almog <killerdrum@gmail.com>
2021-06-19 16:36:43 +0300

    add load more button and page property to breachitem

--- src/components/Breaches.js
+++ src/components/Breaches.js
@@ -4,14 +4,44 @@ import BreachItem from './BreachItem'
 
 const Breaches = () => {
     const [breachesList, setBreachesList] = useState([])
+    const [openBreachId, setOpenBreachId] = useState(null)
+    const [isFetching, setIsFetching] = useState(null) // Either top/bottom/null
 
     useEffect(() => {
         const loadInitialPage = async () => {
+            const page = 0
             const data = await getData()
+            for (const i in data.items) {
+                data.items[i].page = page
+            }
             setBreachesList(data.items)
         }
         loadInitialPage()
     }, [])
+
+    useEffect(() => {
+        if (isFetching !== 'bottom') {
+            return
+        }
+        const fetchMoreData = async () => {
+            const lastItem = breachesList[breachesList.length - 1]
+            const page = lastItem.page + 1
+            const data = await getData(page)
+            for (const i in data.items) {
+                data.items[i].page = page
+            }
+            setBreachesList([...breachesList, ...data.items])
+            setIsFetching(null)
+        }
+        fetchMoreData()
+    }, [isFetching])
+
+    const loadNextPage = () => {
+        if (isFetching) {
+            return
+        }
+        setIsFetching('bottom')
+    }
 
     return (
         <div>
@@ -21,12 +51,18 @@ const Breaches = () => {
                 breachesList.map((item) => {
                     return (
                         <BreachItem
-                            key={item.Name}
+                            key={item.id}
                             breachItem={item}
+                            openBreachId={openBreachId}
                         />
                     )
                 })
             }
+            <button
+                onClick={loadNextPage}
+            >
+                LOAD MORE
+            </button>
         </div>
     )
 }
