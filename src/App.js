2dc6bee07bef62edf6eb1c135298c20f2387af95
Shani Almog <killerdrum@gmail.com>
2021-06-19 18:08:22 +0300

    load theme from user settings and add theme toggle

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
-import './App.css'
+import { useEffect, useState } from 'react'
 import Breaches from './components/Breaches'
+import './App.css'
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles'
+import FormControlLabel from '@material-ui/core/FormControlLabel'
+import Switch from '@material-ui/core/Switch'
 
 function App() {
+  const [palette, setPalette] = useState('light')
+
+  useEffect(() => {
+    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      setPalette('dark')
+    }
+  }, [])
+
+  const onThemeSwitchChange = (e, newValue) => {
+    setPalette(newValue ? 'dark' : 'light')
+  }
+
+  const theme = createMuiTheme({
+    palette: {
+      type: palette
+    }
+  })
+
   return (
     <div>
-      <Breaches />
+      <FormControlLabel
+        control={
+          <Switch
+            checked={palette === 'dark'}
+            onChange={onThemeSwitchChange}
+            color="primary"
+            inputProps={{ 'aria-label': 'primary checkbox' }}
+          />
+        }
+        label="Dark Mode"
+      />
+      <ThemeProvider theme={theme}>
+        <Breaches />
+      </ThemeProvider>
     </div>
   )
 }
